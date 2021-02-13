b7a4fc183f888943f6c9aec002cd6bee9869b6ba
Andy Goodrich <acg.node@gmail.com>
2021-02-13 15:16:11 -0500

    Andy Goodrich: pick up Fred Doucet's modifications to the test.

--- tests/systemc/misc/cae_test/general/bitwise/shl/datatypes/datatypes.cpp
+++ tests/systemc/misc/cae_test/general/bitwise/shl/datatypes/datatypes.cpp
@@ -19,7 +19,7 @@
 
 /*****************************************************************************
 
-  datatypes.cpp -- 
+  datatypes.cpp --
 
   Original Author: Rocco Jonack, Synopsys, Inc., 1999-07-30
 
@@ -37,13 +37,20 @@
 
 
 #include "datatypes.h"
-#define true 1
-#define false  0
+
+
+template<typename T_IN, typename SHIFT_T, typename T_OUT>
+void do_shift(const T_IN& tmp, SHIFT_T shift, T_OUT& tmpr)
+{
+   if (shift>=0) {
+     tmpr = tmp << shift;
+   } else {
+     tmpr = tmp >> -shift;
+   }
+}
 
 void datatypes::entry()
-
 {
-
   sc_bigint<8>    tmp1;
   sc_bigint<8>    tmp1r;
   sc_biguint<8>   tmp2;
@@ -59,8 +66,6 @@ void datatypes::entry()
 
 // define 1 dimensional array
    unsigned int  tmp7[2];
-   char tmp8[2];
-
 // define sc_bool_vector
   sc_bv<4>	tmp10;
   tmp10[3] = 0;  tmp10[2] = 1;  tmp10[1] = 0;  tmp10[0] = 1;
@@ -83,8 +88,6 @@ void datatypes::entry()
 
    tmp7[0] = 3;
    tmp7[1] = 12;
-   tmp8[0] = 'S';
-   tmp8[1] = 'C';
    tmp11[0][0] = "1";
    tmp11[1][0] = "0";
 
@@ -99,16 +102,23 @@ void datatypes::entry()
     tmp4 = in_value4.read();
     tmp5 = in_value5.read();
     tmp6 = in_value6.read();
-    
+
     out_ack.write(true);
 
-    //execute mixed data type shift left operations
-    tmp1r = tmp1 << (tmp7[0] % 8);
-    tmp2r = tmp2 << 2;	
-    tmp3r = tmp3 << 1;
-    tmp4r = tmp4 << (tmp7[1] % 16);
-    tmp5r = tmp3 << ((unsigned int)(tmp1.to_int()) % 32);
-    tmp6r = tmp6 << 1;
+    //execute mixed data type shift operations
+    sc_bigint<8>  shift1 = (tmp7[0] % 8);
+    sc_biguint<8> shift2 = 2;
+    long          shift3 = 1;
+    int           shift4 = (tmp7[1] % 16);
+    short         shift5 = ((unsigned int)(tmp1.to_int()) % 32);
+    char          shift6 = 1;
+
+    do_shift(tmp1,shift1,tmp1r);
+    do_shift(tmp2,shift2,tmp2r);
+    do_shift(tmp3,shift3,tmp3r);
+    do_shift(tmp4,shift4,tmp4r);
+    do_shift(tmp3,shift5,tmp5r);
+    do_shift(tmp6,shift6,tmp6r);
 
     //write outputs
     out_value1.write(tmp1r);
@@ -117,20 +127,27 @@ void datatypes::entry()
     out_value4.write(tmp4r);
     out_value5.write(tmp5r);
     out_value6.write(tmp6r);
-  
+
     out_valid.write(true);
     wait();
     out_ack.write(false);
     out_valid.write(false);
 
-    //execute mixed data type shift left operations
-    tmp1r = tmp1 << (tmp7[0] % 8);
-    tmp2r = tmp2 << (unsigned int)(tmp4 % 8);
-    tmp3r = tmp3 << (short)(tmp5 % 32);
-    tmp4r = tmp4 << 2;
-    tmp5r = tmp3 << ((unsigned int)(tmp5) % 16);
-    tmp6r = tmp6 << (tmp2.to_uint() % 32);
- 
+    shift1 = (tmp7[0] % 8);
+    shift2 = (unsigned int)(tmp4 % 8);
+    shift3 = (short)(tmp5 % 32);
+    shift4 = 2;
+    shift5 = ((unsigned int)(tmp5) % 16);
+    shift6 = (tmp2.to_uint() % 32);
+
+    //execute mixed data type shift operations
+    do_shift(tmp1,shift1,tmp1r);
+    do_shift(tmp2,shift2,tmp2r);
+    do_shift(tmp3,shift3,tmp3r);
+    do_shift(tmp4,shift4,tmp4r);
+    do_shift(tmp3,shift5,tmp5r);
+    do_shift(tmp6,shift6,tmp6r);
+
     //write outputs
     out_value1.write(tmp1r);
     out_value2.write(tmp2r);
@@ -138,7 +155,7 @@ void datatypes::entry()
     out_value4.write(tmp4r);
     out_value5.write(tmp5r);
     out_value6.write(tmp6r);
- 
+
     out_valid.write(true);
     wait();
     out_ack.write(false);
@@ -146,5 +163,5 @@ void datatypes::entry()
 
  }
 
-} // End 
+} // End
 
