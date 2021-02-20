0e53cc7a95c205bcaaa012418c047782320fc74a
Andy Goodrich <acg.node@gmail.com>
2021-02-20 12:47:38 -0500

    Andy Goodrich: improvements to debug messaging.

--- tests/systemc/datatypes/int/sc_bigint/part_selects/part_selects.cpp
+++ tests/systemc/datatypes/int/sc_bigint/part_selects/part_selects.cpp
@@ -10,13 +10,15 @@ using std::hex;
 using std::dec;
 #include "systemc.h"
 
+#define DEBUGF false
+
 template<int W>
 class Selection : public Selection<W-1>
 {
   public:
     Selection( unsigned int fill ) : Selection<W-1>(fill)
     {
-        sc_biguint<W> source;
+        sc_bigint<W> source;
 	source = fill;
 	if ( W > 32 ) { source = (source << 32) | fill; }
 	if ( W > 64 ) { source = (source << 32) | fill; }
@@ -25,21 +27,22 @@ class Selection : public Selection<W-1>
 	for ( int low = 0; low < W; ++low ) {
 	    for ( int high = low; high < W; ++high ) {
 	        int width = (high-low) + 1;
-		sc_bigint<W> mask = -1;
-		if (0) std::cout << std::endl << "sc_biguint<" << W << ">(" << high << "," << low << "):" << endl;
+		sc_biguint<W> mask = -1;
+		if (DEBUGF) std::cout << std::endl << "sc_bigint<" << W << ">(" << high << "," << low << "):" << endl;
 		mask = ~(mask << width);
 		sc_unsigned actual = source(high,low);
-		sc_unsigned shift = (source >> low);
-		sc_unsigned expected = (source >> low) & mask;
-		if (0) std::cout << "shift & mask = expected" << std::endl;
-		if (0) std::cout << std::hex << shift << " & " << mask << " = " << expected << std::dec << std::endl;
+		sc_unsigned shift = (sc_unsigned(source) >> low);
+		sc_unsigned expected = shift & mask;
+		if (DEBUGF) std::cout << "shift & mask = expected" << std::endl;
+		if (DEBUGF) std::cout << std::hex << shift << " & " << mask << " = " << expected << std::dec << std::endl;
 		if ( actual != expected ) {
-		    cout << "ERROR sc_biguint<" << W << ">(" << high << "," << low << "):" << endl;
-		    cout << "  expected " << hex << expected << dec << " " << expected.get_hod() << endl;
-		    cout << "  actual   " << hex << actual << dec << " " << actual.get_hod() << endl;
-		    cout << "  mask     " << hex << mask << dec << " " << mask.get_hod() << endl;
+		    cout << "ERROR sc_bigint<" << W << ">(" << high << "," << low << "):" << endl;
+		    cout << "  expected " << hex << expected << dec << endl;
+		    cout << "  actual   " << hex << actual << dec <<  endl;
+		    cout << "  mask     " << hex << mask << dec << endl;
 		    cout << "  shift    " << hex << shift << endl;
-		    cout << "  source   " << hex << source << endl;
+		    cout << "  source   " << hex << source << dec << endl;
+		    assert(0);
 		}
 	    }
         }
