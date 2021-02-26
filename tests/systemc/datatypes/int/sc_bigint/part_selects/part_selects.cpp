c29a78f6841721f44d34a8039470c6e1291a1859
Andy Goodrich <acg.node@gmail.com>
2021-02-26 06:43:49 -0500

    Andy Goodrich: added writes to part selections in addition to reads from them in test.

--- tests/systemc/datatypes/int/sc_bigint/part_selects/part_selects.cpp
+++ tests/systemc/datatypes/int/sc_bigint/part_selects/part_selects.cpp
@@ -125,37 +125,125 @@ class Selection<0,D>
     Selection(unsigned int fill ) {}
 };
 
+template<int W, int D=1>
+class SelectionWrite : public SelectionWrite<W-D,D>
+{
+  public:
+    SelectionWrite( unsigned int fill ) : SelectionWrite<W-D,D>(fill)
+    {
+        sc_bigint<W> bigint_actual;
+        sc_bigint<W> bigint_expected;
+        sc_bigint<W> bigint_source;
+        sc_signed    signed_actual(W);
+        sc_signed    signed_expected(W);
+        sc_signed    signed_source(W);
+
+	size_t width = (W+31)/32;
+	for ( size_t data_i = 0; data_i < width; ++data_i ) {
+	    bigint_source = (bigint_source << 32) | fill; 
+	    signed_source = (signed_source << 32) | fill; 
+	}
+	
+	for ( int low = 0; low < W; ++low ) {
+	    for ( int high = low; high < W; ++high ) {
+	        int width = (high-low) + 1;
+		sc_biguint<W> mask = -1; 
+		mask = ~(mask << width);
+
+		// test sc_biguint<W> selection:
+
+                if ( 1 )
+		{
+		    bigint_actual = 0;
+		    bigint_actual(high,low) = bigint_source;
+		    bigint_expected = bigint_source & mask;
+		    bigint_expected = bigint_expected << low;
+		    if ( bigint_actual != bigint_expected ) {
+			cout << "ERROR write to sc_biguint<" << W << ">(" << high << "," << low 
+                             << "):" << endl;
+			cout << "  width    " << width << endl;
+			dump( "  expected ", bigint_expected );
+			dump( "  actual   " , bigint_actual );
+			dump( "  mask     ", mask );
+			dump( "  source   ", bigint_source );
+			assert(0);
+		    }
+		}
+
+		// test sc_unsigned selection:
+
+		{
+		    signed_actual = 0;
+		    signed_actual(high,low) = signed_source;
+		    signed_expected = signed_source & mask;
+		    signed_expected = signed_expected << low;
+		    if ( bigint_actual != bigint_expected ) {
+			cout << "ERROR write to sc_signed(" << W << ")(" << high << "," << low 
+                             << "):" << endl;
+			cout << "  width    " << width << endl;
+			dump( "  expected ", signed_expected );
+			dump( "  actual   " , signed_actual );
+			dump( "  mask     ", mask );
+			dump( "  source   ", signed_source );
+			assert(0);
+		    }
+		}
+
+	    }
+        }
+    }
+};
+
+template<int D>
+class SelectionWrite<0,D>
+{
+  public:
+    SelectionWrite(unsigned int fill ) {}
+};
+
 int sc_main( int argc, char* argv[] )
 {
     if ( true )
     {
 	cout << "Selection(0x" << std::hex << ~0u << std::dec << ")" << std::endl;
 	Selection<128> x(~0u);
-	Selection<2000,200> y1(~0u);
+	Selection<2000,200> y(~0u);
+
+	cout << "SelectionWrite(0x" << std::hex << ~0u << std::dec << ")" << std::endl;
+	SelectionWrite<128> x_write(~0u);
+	SelectionWrite<2000,200> y_write(~0u);
     }
     if ( true )
     {
 	cout << "Selection(0x" << std::hex << 0x55555555u << std::dec << ")" << std::endl;
-	Selection<128> x1(0x55555555u);
-	Selection<2000,200> y(0x55555555u);
+	Selection<128> x(0x55555555u);
+
+	cout << "SelectionWrite(0x" << std::hex << 0x55555555u << std::dec << ")" << std::endl;
+	SelectionWrite<128> x_write1(0x55555555u);
     }
     if ( true )
     {
 	cout << "Selection(0x" << std::hex << 0xaaaaaaaau << std::dec << ")" << std::endl;
-	Selection<128> x2(0xaaaaaaaau);
-	Selection<1200,120> y2(0xaaaaaaaau);
+	Selection<128> x(0xaaaaaaaau);
+
+	cout << "Selection(0x" << std::hex << 0xaaaaaaaau << std::dec << ")" << std::endl;
+	SelectionWrite<128> x_write(0xaaaaaaaau);
     }
     if ( true )
     {
 	cout << "Selection(0x" << std::hex << 0x99999999u << std::dec << ")" << std::endl;
-	Selection<128> x1(0x99999999u);
-	Selection<1200,120> y2(0x99999999u);
+	Selection<128> x(0x99999999u);
+
+	cout << "SelectionWrite(0x" << std::hex << 0x99999999u << std::dec << ")" << std::endl;
+	SelectionWrite<128> x_write(0x99999999u);
     }
     if ( true )
     {
 	cout << "Selection(0x" << std::hex << 0x66666666u << std::dec << ")" << std::endl;
-	Selection<128> x1(0x66666666u);
-	Selection<1200,120> y2(0x66666666u);
+	Selection<128> x(0x66666666u);
+
+	cout << "SelectionWrite(0x" << std::hex << 0x66666666u << std::dec << ")" << std::endl;
+	SelectionWrite<128> x_write(0x66666666u);
     }
     cout << "Program completed" << endl;
 
--- tests/systemc/datatypes/int/sc_biguint/part_selects/part_selects.cpp
+++ tests/systemc/datatypes/int/sc_biguint/part_selects/part_selects.cpp
@@ -122,37 +122,127 @@ class Selection<0,D>
     Selection(unsigned int fill ) {}
 };
 
+template<int W, int D=1>
+class SelectionWrite : public SelectionWrite<W-D,D>
+{
+  public:
+    SelectionWrite( unsigned int fill ) : SelectionWrite<W-D,D>(fill)
+    {
+        sc_biguint<W> biguint_actual;
+        sc_biguint<W> biguint_expected;
+        sc_biguint<W> biguint_source;
+        sc_unsigned   signed_actual(W);
+        sc_unsigned   signed_expected(W);
+        sc_unsigned   signed_source(W);
+
+	size_t width = (W+31)/32;
+	for ( size_t data_i = 0; data_i < width; ++data_i ) {
+	    biguint_source = (biguint_source << 32) | fill; 
+	    signed_source = (signed_source << 32) | fill; 
+	}
+	
+	for ( int low = 0; low < W; ++low ) {
+	    for ( int high = low; high < W; ++high ) {
+	        int width = (high-low) + 1;
+		sc_biguint<W> mask = -1; 
+		mask = ~(mask << width);
+
+		// test sc_biguint<W> selection:
+
+                if ( 1 )
+		{
+		    biguint_actual = 0;
+		    biguint_actual(high,low) = biguint_source;
+		    biguint_expected = biguint_source & mask;
+		    biguint_expected = biguint_expected << low;
+		    if ( biguint_actual != biguint_expected ) {
+			cout << "ERROR write to sc_biguint<" << W << ">(" << high << "," << low 
+                             << "):" << endl;
+			cout << "  width    " << width << endl;
+			dump( "  expected ", biguint_expected );
+			dump( "  actual   " , biguint_actual );
+			dump( "  mask     ", mask );
+			dump( "  source   ", biguint_source );
+			assert(0);
+		    }
+		}
+
+		// test sc_unsigned selection:
+
+		{
+		    signed_actual = 0;
+		    signed_actual(high,low) = signed_source;
+		    signed_expected = signed_source & mask;
+		    signed_expected = signed_expected << low;
+		    if ( biguint_actual != biguint_expected ) {
+			cout << "ERROR write to sc_unsigned(" << W << ")(" << high << "," << low 
+                             << "):" << endl;
+			cout << "  width    " << width << endl;
+			dump( "  expected ", signed_expected );
+			dump( "  actual   " , signed_actual );
+			dump( "  mask     ", mask );
+			dump( "  source   ", signed_source );
+			assert(0);
+		    }
+		}
+
+	    }
+        }
+    }
+};
+
+template<int D>
+class SelectionWrite<0,D>
+{
+  public:
+    SelectionWrite(unsigned int fill ) {}
+};
+
 int sc_main( int argc, char* argv[] )
 {
     if ( true )
     {
 	cout << "Selection(0x" << std::hex << ~0u << std::dec << ")" << std::endl;
 	Selection<128> x(~0u);
-	Selection<2000,200> y1(~0u);
+	Selection<2000,200> y(~0u);
+
+	cout << "SelectionWrite(0x" << std::hex << ~0u << std::dec << ")" << std::endl;
+	SelectionWrite<128> x_write(~0u);
+	SelectionWrite<2000,200> y_write(~0u);
     }
     if ( true )
     {
 	cout << "Selection(0x" << std::hex << 0x55555555u << std::dec << ")" << std::endl;
-	Selection<128> x1(0x55555555u);
-	Selection<2000,200> y1(0x55555555u);
+	Selection<128> x(0x55555555u);
+	Selection<2000,200> y(0x55555555u);
+
+	cout << "SelectionWrite(0x" << std::hex << 0x55555555u << std::dec << ")" << std::endl;
+	SelectionWrite<128> x_write1(0x55555555u);
+	SelectionWrite<2000,200> y_write(0x55555555u);
     }
     if ( true )
     {
 	cout << "Selection(0x" << std::hex << 0xaaaaaaaau << std::dec << ")" << std::endl;
-	Selection<128> x2(0xaaaaaaaau);
-	Selection<1200,120> y2(0xaaaaaaaau);
+	Selection<128> x(0xaaaaaaaau);
+
+	cout << "Selection(0x" << std::hex << 0xaaaaaaaau << std::dec << ")" << std::endl;
+	SelectionWrite<128> x_write(0xaaaaaaaau);
     }
     if ( true )
     {
 	cout << "Selection(0x" << std::hex << 0x99999999u << std::dec << ")" << std::endl;
-	Selection<128> x1(0x99999999u);
-	Selection<1200,120> y2(0x99999999u);
+	Selection<128> x(0x99999999u);
+
+	cout << "SelectionWrite(0x" << std::hex << 0x99999999u << std::dec << ")" << std::endl;
+	SelectionWrite<128> x_write(0x99999999u);
     }
     if ( true )
     {
 	cout << "Selection(0x" << std::hex << 0x66666666u << std::dec << ")" << std::endl;
-	Selection<128> x1(0x66666666u);
-	Selection<1200,120> y2(0x66666666u);
+	Selection<128> x(0x66666666u);
+
+	cout << "SelectionWrite(0x" << std::hex << 0x66666666u << std::dec << ")" << std::endl;
+	SelectionWrite<128> x_write(0x66666666u);
     }
     cout << "Program completed" << endl;
 
