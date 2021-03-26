90ef0fcc5ddf82dc8ae77f2a91f175d0c68fdb68
Andy Goodrich <acg.node@gmail.com>
2021-03-26 01:19:56 -0400

    Andy Goodrich: add test with uint64 values. Expand the number of times tests are run with random values.

--- tests/systemc/datatypes/bit/sc_bit/test02/test02.cpp
+++ tests/systemc/datatypes/bit/sc_bit/test02/test02.cpp
@@ -1,6 +1,8 @@
 #include "systemc.h"
 #include "isaac.h"
 
+#define COUNT 100
+
 QTIsaac<8> rng;         // Platform independent random number generator.
 
 #define DUMP( WHAT ) \
@@ -8,7 +10,31 @@ QTIsaac<8> rng;         // Platform independent random number generator.
     cout << hex << WHAT << " " << #WHAT << " ( " << dec << WHAT.length() << " ) " << endl; \
 }
 
-#define COMPARE_BV( LEFT, RIGHT ) \
+#define COMPARE_UINT64_BV( LEFT, RIGHT ) \
+    if ( ( LEFT != RIGHT ) ) { \
+	cout << endl << "ERROR DETECTED @ " << __FILE__ << ":" << __LINE__ << ":" << endl; \
+	DUMP(v_sc_unsigned); \
+	DUMP(v_sc_bv_base); \
+	cout << hex << v_uint64 << dec << " v_uint64 " << endl; \
+	cout << endl; \
+	DUMP(LEFT); \
+	DUMP(RIGHT); \
+	cout << endl; \
+    }
+
+#define COMPARE_UINT64_LV( LEFT, RIGHT ) \
+    if ( ( LEFT != RIGHT ) ) { \
+	cout << endl << "ERROR DETECTED @ " << __FILE__ << ":" << __LINE__ << ":" << endl; \
+	DUMP(v_sc_unsigned); \
+	DUMP(v_sc_lv_base); \
+	cout << hex << v_uint64 << dec << " v_uint64 " << endl; \
+	cout << endl; \
+	DUMP(LEFT); \
+	DUMP(RIGHT); \
+	cout << endl; \
+    }
+
+#define COMPARE_UNSIGNED_BV( LEFT, RIGHT ) \
     if ( ( LEFT != RIGHT ) ) { \
 	cout << endl << "ERROR DETECTED @ " << __FILE__ << ":" << __LINE__ << ":" << endl; \
 	DUMP(left_sc_unsigned); \
@@ -21,7 +47,7 @@ QTIsaac<8> rng;         // Platform independent random number generator.
 	cout << endl; \
     }
 
-#define COMPARE_LV( LEFT, RIGHT ) \
+#define COMPARE_UNSIGNED_LV( LEFT, RIGHT ) \
     if ( ( LEFT != RIGHT ) ) { \
 	cout << endl << "ERROR DETECTED @ " << __FILE__ << ":" << __LINE__ << ":" << endl; \
 	DUMP(left_sc_unsigned); \
@@ -53,9 +79,9 @@ void test_and( const sc_unsigned& left_sc_unsigned, const sc_unsigned& right_sc_
     sc_unsigned left_lv_or_right_unsigned ( left_sc_lv_base & right_sc_unsigned );
     sc_unsigned left_lv_or_right_lv( left_sc_lv_base & right_sc_lv_base );
     
-    COMPARE_LV( left_unsigned_or_right_unsigned, left_unsigned_or_right_lv )
-    COMPARE_LV( left_unsigned_or_right_unsigned, left_lv_or_right_unsigned )
-    COMPARE_LV( left_unsigned_or_right_unsigned, left_lv_or_right_lv )
+    COMPARE_UNSIGNED_LV( left_unsigned_or_right_unsigned, left_unsigned_or_right_lv )
+    COMPARE_UNSIGNED_LV( left_unsigned_or_right_unsigned, left_lv_or_right_unsigned )
+    COMPARE_UNSIGNED_LV( left_unsigned_or_right_unsigned, left_lv_or_right_lv )
 }
 
 void test_and( const sc_unsigned& left_sc_unsigned, const sc_unsigned& right_sc_unsigned,
@@ -66,9 +92,9 @@ void test_and( const sc_unsigned& left_sc_unsigned, const sc_unsigned& right_sc_
     sc_unsigned left_bv_or_right_unsigned ( left_sc_bv_base & right_sc_unsigned );
     sc_unsigned left_bv_or_right_bv( left_sc_bv_base & right_sc_bv_base );
     
-    COMPARE_BV( left_unsigned_or_right_unsigned, left_unsigned_or_right_bv )
-    COMPARE_BV( left_unsigned_or_right_unsigned, left_bv_or_right_unsigned )
-    COMPARE_BV( left_unsigned_or_right_unsigned, left_bv_or_right_bv )
+    COMPARE_UNSIGNED_BV( left_unsigned_or_right_unsigned, left_unsigned_or_right_bv )
+    COMPARE_UNSIGNED_BV( left_unsigned_or_right_unsigned, left_bv_or_right_unsigned )
+    COMPARE_UNSIGNED_BV( left_unsigned_or_right_unsigned, left_bv_or_right_bv )
 }
 
 void test_or( const sc_unsigned& left_sc_unsigned, const sc_unsigned& right_sc_unsigned,
@@ -80,9 +106,9 @@ void test_or( const sc_unsigned& left_sc_unsigned, const sc_unsigned& right_sc_u
     sc_unsigned left_lv_or_right_lv( left_sc_lv_base | right_sc_lv_base );
     
 
-    COMPARE_LV( left_unsigned_or_right_unsigned, left_unsigned_or_right_lv )
-    COMPARE_LV( left_unsigned_or_right_unsigned, left_lv_or_right_unsigned )
-    COMPARE_LV( left_unsigned_or_right_unsigned, left_lv_or_right_lv )
+    COMPARE_UNSIGNED_LV( left_unsigned_or_right_unsigned, left_unsigned_or_right_lv )
+    COMPARE_UNSIGNED_LV( left_unsigned_or_right_unsigned, left_lv_or_right_unsigned )
+    COMPARE_UNSIGNED_LV( left_unsigned_or_right_unsigned, left_lv_or_right_lv )
 }
 
 void test_or( const sc_unsigned& left_sc_unsigned, const sc_unsigned& right_sc_unsigned,
@@ -94,9 +120,9 @@ void test_or( const sc_unsigned& left_sc_unsigned, const sc_unsigned& right_sc_u
     sc_unsigned left_bv_or_right_bv( left_sc_bv_base | right_sc_bv_base );
     
 
-    COMPARE_BV( left_unsigned_or_right_unsigned, left_unsigned_or_right_bv )
-    COMPARE_BV( left_unsigned_or_right_unsigned, left_bv_or_right_unsigned )
-    COMPARE_BV( left_unsigned_or_right_unsigned, left_bv_or_right_bv )
+    COMPARE_UNSIGNED_BV( left_unsigned_or_right_unsigned, left_unsigned_or_right_bv )
+    COMPARE_UNSIGNED_BV( left_unsigned_or_right_unsigned, left_bv_or_right_unsigned )
+    COMPARE_UNSIGNED_BV( left_unsigned_or_right_unsigned, left_bv_or_right_bv )
 }
 
 void test_xor( const sc_unsigned& left_sc_unsigned, const sc_unsigned& right_sc_unsigned,
@@ -107,9 +133,9 @@ void test_xor( const sc_unsigned& left_sc_unsigned, const sc_unsigned& right_sc_
     sc_unsigned left_lv_or_right_unsigned ( left_sc_lv_base ^ right_sc_unsigned );
     sc_unsigned left_lv_or_right_lv( left_sc_lv_base ^ right_sc_lv_base );
     
-    COMPARE_LV( left_unsigned_or_right_unsigned, left_unsigned_or_right_lv )
-    COMPARE_LV( left_unsigned_or_right_unsigned, left_lv_or_right_unsigned )
-    COMPARE_LV( left_unsigned_or_right_unsigned, left_lv_or_right_lv )
+    COMPARE_UNSIGNED_LV( left_unsigned_or_right_unsigned, left_unsigned_or_right_lv )
+    COMPARE_UNSIGNED_LV( left_unsigned_or_right_unsigned, left_lv_or_right_unsigned )
+    COMPARE_UNSIGNED_LV( left_unsigned_or_right_unsigned, left_lv_or_right_lv )
 }
 
 void test_xor( const sc_unsigned& left_sc_unsigned, const sc_unsigned& right_sc_unsigned,
@@ -120,9 +146,9 @@ void test_xor( const sc_unsigned& left_sc_unsigned, const sc_unsigned& right_sc_
     sc_unsigned left_bv_or_right_unsigned ( left_sc_bv_base ^ right_sc_unsigned );
     sc_unsigned left_bv_or_right_bv( left_sc_bv_base ^ right_sc_bv_base );
     
-    COMPARE_BV( left_unsigned_or_right_unsigned, left_unsigned_or_right_bv )
-    COMPARE_BV( left_unsigned_or_right_unsigned, left_bv_or_right_unsigned )
-    COMPARE_BV( left_unsigned_or_right_unsigned, left_bv_or_right_bv )
+    COMPARE_UNSIGNED_BV( left_unsigned_or_right_unsigned, left_unsigned_or_right_bv )
+    COMPARE_UNSIGNED_BV( left_unsigned_or_right_unsigned, left_bv_or_right_unsigned )
+    COMPARE_UNSIGNED_BV( left_unsigned_or_right_unsigned, left_bv_or_right_bv )
 }
 
 void test1()
@@ -136,28 +162,91 @@ void test1()
 	    sc_lv_base  left_sc_lv_base(left_i);
 	    sc_lv_base  right_sc_lv_base(right_i);
 
-            load_rand( left_i, left_sc_unsigned );
-            load_rand( right_i, right_sc_unsigned );
+	    for ( size_t count_i = 0; count_i < COUNT; ++count_i ) {
+		load_rand( left_i, left_sc_unsigned );
+		load_rand( right_i, right_sc_unsigned );
 
-            left_sc_lv_base = left_sc_unsigned;
-            right_sc_lv_base = right_sc_unsigned;
-	    test_and( left_sc_unsigned, right_sc_unsigned, left_sc_lv_base, right_sc_lv_base );
-	    test_or( left_sc_unsigned, right_sc_unsigned, left_sc_lv_base, right_sc_lv_base );
-	    test_xor( left_sc_unsigned, right_sc_unsigned, left_sc_lv_base, right_sc_lv_base );
+		left_sc_lv_base = left_sc_unsigned;
+		right_sc_lv_base = right_sc_unsigned;
+		test_and( left_sc_unsigned, right_sc_unsigned, left_sc_lv_base, right_sc_lv_base );
+		test_or( left_sc_unsigned, right_sc_unsigned, left_sc_lv_base, right_sc_lv_base );
+		test_xor( left_sc_unsigned, right_sc_unsigned, left_sc_lv_base, right_sc_lv_base );
 
-            left_sc_bv_base = left_sc_unsigned;
-            right_sc_bv_base = right_sc_unsigned;
-	    test_and( left_sc_unsigned, right_sc_unsigned, left_sc_bv_base, right_sc_bv_base );
-	    test_or( left_sc_unsigned, right_sc_unsigned, left_sc_bv_base, right_sc_bv_base );
-	    test_xor( left_sc_unsigned, right_sc_unsigned, left_sc_bv_base, right_sc_bv_base );
-
+		left_sc_bv_base = left_sc_unsigned;
+		right_sc_bv_base = right_sc_unsigned;
+		test_and( left_sc_unsigned, right_sc_unsigned, left_sc_bv_base, right_sc_bv_base );
+		test_or( left_sc_unsigned, right_sc_unsigned, left_sc_bv_base, right_sc_bv_base );
+		test_xor( left_sc_unsigned, right_sc_unsigned, left_sc_bv_base, right_sc_bv_base );
+	    }
         }
     }
+}
+
+void test2()
+{
+    for ( int width_i = 1; width_i < 128; ++width_i ) {
+        sc_bv_base  v_sc_bv_base(width_i);
+        sc_lv_base  v_sc_lv_base(width_i);
+        sc_unsigned v_sc_unsigned(width_i);
+	uint64      v_uint64;
+
+	for ( size_t count_i = 0; count_i < COUNT; ++count_i ) {
+	    load_rand( width_i, v_sc_unsigned );
+	    v_sc_lv_base = v_sc_unsigned;
+	    v_sc_bv_base = v_sc_unsigned;
+	    v_uint64 = rng.rand();
+	    v_uint64 = (v_uint64 << 32) | rng.rand();
+
+	    // Test &
+
+	    sc_unsigned v_uint64_and_sc_unsigned( v_uint64 & v_sc_unsigned );
+	    sc_unsigned v_uint64_and_sc_bv_base( v_uint64 & v_sc_bv_base );
+	    sc_unsigned v_uint64_and_sc_lv_base( v_uint64 & v_sc_lv_base ); 
+	    COMPARE_UINT64_BV( v_uint64_and_sc_unsigned, v_uint64_and_sc_bv_base ) 
+	    COMPARE_UINT64_LV( v_uint64_and_sc_unsigned, v_uint64_and_sc_lv_base ) 
+
+	    sc_unsigned v_sc_unsigned_and_uint64( v_sc_unsigned & v_uint64 );
+	    sc_unsigned v_sc_bv_base_and_uint64( v_sc_bv_base & v_uint64 );
+	    sc_unsigned v_sc_lv_base_and_uint64( v_sc_lv_base & v_uint64 );
+	    COMPARE_UINT64_BV( v_sc_unsigned_and_uint64, v_sc_bv_base_and_uint64 ) 
+	    COMPARE_UINT64_LV( v_sc_unsigned_and_uint64, v_sc_lv_base_and_uint64 ) 
+
+	    // Test |
+
+	    sc_unsigned v_uint64_or_sc_unsigned( v_uint64 | v_sc_unsigned );
+	    sc_unsigned v_uint64_or_sc_bv_base( v_uint64 | v_sc_bv_base );
+	    sc_unsigned v_uint64_or_sc_lv_base( v_uint64 | v_sc_lv_base ); 
+	    COMPARE_UINT64_BV( v_uint64_or_sc_unsigned, v_uint64_or_sc_bv_base ) 
+	    COMPARE_UINT64_LV( v_uint64_or_sc_unsigned, v_uint64_or_sc_lv_base ) 
+
+	    sc_unsigned v_sc_unsigned_or_uint64( v_sc_unsigned | v_uint64 );
+	    sc_unsigned v_sc_bv_base_or_uint64( v_sc_bv_base | v_uint64 );
+	    sc_unsigned v_sc_lv_base_or_uint64( v_sc_lv_base | v_uint64 );
+	    COMPARE_UINT64_BV( v_sc_unsigned_or_uint64, v_sc_bv_base_or_uint64 ) 
+	    COMPARE_UINT64_LV( v_sc_unsigned_or_uint64, v_sc_lv_base_or_uint64 ) 
+
+	    // Test ^
+
+	    sc_unsigned v_uint64_xor_sc_unsigned( v_uint64 ^ v_sc_unsigned );
+	    sc_unsigned v_uint64_xor_sc_bv_base( v_uint64 ^ v_sc_bv_base );
+	    sc_unsigned v_uint64_xor_sc_lv_base( v_uint64 ^ v_sc_lv_base ); 
+	    COMPARE_UINT64_BV( v_uint64_xor_sc_unsigned, v_uint64_xor_sc_bv_base ) 
+	    COMPARE_UINT64_LV( v_uint64_xor_sc_unsigned, v_uint64_xor_sc_lv_base ) 
+
+	    sc_unsigned v_sc_unsigned_xor_uint64( v_sc_unsigned ^ v_uint64 );
+	    sc_unsigned v_sc_bv_base_xor_uint64( v_sc_bv_base ^ v_uint64 );
+	    sc_unsigned v_sc_lv_base_xor_uint64( v_sc_lv_base ^ v_uint64 );
+	    COMPARE_UINT64_BV( v_sc_unsigned_xor_uint64, v_sc_bv_base_xor_uint64 ) 
+	    COMPARE_UINT64_LV( v_sc_unsigned_xor_uint64, v_sc_lv_base_xor_uint64 ) 
+        }
+    }
+	
 }
 
 int sc_main(int argc, char* argv[])
 {
     test1();
+    test2();
 
     cout << "Bit logic operators test completed" << endl;
     return 0;
